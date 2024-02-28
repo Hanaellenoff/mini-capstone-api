43c2972616bed8dfad0facb5d5be7b1913c95a3f
Hanaellenoff <hanaellenoff@gmail.com>
2024-02-28 13:06:08 -0600

    added a lot

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,20 @@ class ProductsController < ApplicationController
     render :show
   end 
   def index
-    @products = Product.find_by(id: params[:id])
+    @products = Product.all
     render :index
+  end 
+  def show
+    @product = Product.find_by(id: params[:id])
+    render :show
+  end 
+  def create
+    @product = Product.create(
+    name: params["icecube"],
+    price: params[2],
+    image_url: params["http://google.com"],
+    description: params["really cold"]
+    )
+    render :show
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "/one_product/:id", controller: "products", action: "one_product"
+   get "/products/:id", controller: "products", action: "show"
   get "/products" => "products#index"
+  post "/products" => "products#create"
   # Defines the root path route ("/")
   # root "posts#index"
 end
