27651cd61ea94b3d2c2d76853d1521491c19b119
Hanaellenoff <hanaellenoff@gmail.com>
2024-03-25 13:56:24 -0500

    make sure your logged in

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,29 +1,47 @@
 class OrdersController < ApplicationController
  
   def index
-    @orders =  current_user.orders.all
-    render :index
+    if current_user 
+      @orders =  current_user.orders
+      render :index
+    else
+      render json: [], status: :unauthorized
+    end 
   end 
 
   def show
-    @order = current_user.orders.find_by(id: params[:id])
-    render :show
+    if current_user
+      @order = current_user.orders.find_by(id: params[:id])
+      render :show
+    else
+      redner json: [], staus: :unauthorized
+    end 
   end 
 
   def create
-    @order = Order.create(
-      user_id: current_user.id,
-      product_id: params["product_id"],
-      quantity: params["quantity"],
-      # subtotal: params["subtotal"],
-      # tax: params["tax"],
-      # total: params["total"]
-    )
-    if @order.save
-      render :show
+    if current_user
+      @order = Order.create(
+        user_id: current_user.id,
+        product_id: params["product_id"],
+        quantity: params["quantity"],
+        # subtotal: params["subtotal"],
+        # tax: params["tax"],
+        # total: params["total"]
+      )
+      if @order.save
+        render :show
+      else
+        render json: { errors: @order.errors.full_messages },
+        status: 22 
+      end 
     else
-      render json: { errors: @order.errors.full_messages },
-      status: 22 
-    end 
+      render json: [], status: :unauthorized
+    end  
+  end 
+  # in process
+  def update
+    @order = Order.update(
+      product_id: 
+    )
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,5 @@ Rails.application.routes.draw do
   post "/orders" => "orders#create"
   get "/orders" => "orders#index"
   get "/order/:id" => "orders#show"
+  patch "/order/:id" => "orders#update"
 end
