0bf58c06979d4e73cea9602bc64e31354022a158
Hanaellenoff <hanaellenoff@gmail.com>
2024-04-05 12:24:16 -0500

    edited cors

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
-  belongs_to :product
+  has_many :carted_products
   def subtotal
     return product.price
   end 
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,18 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com", "mini-capstone-frontend"
+    origins "example.com", "mini-capstone-frontend", "localhost:5173"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
 
 
 
-# create all the models
-# migrate the db 
-# make assosiations
-# make controllers and routes 
-# make a show/index action for carted products
-# make sure only the user can see their cart
-# meaning they need to be logged in
-# make a create action for carted oroducts so users can add to cart 
-#  and a destroy to take things out
