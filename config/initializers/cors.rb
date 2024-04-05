f8e00b031828f3f681889331ee8d94a6441a130f
Hanaellenoff <hanaellenoff@gmail.com>
2024-04-04 22:02:00 -0400

    made cartedproducts table

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,3 +4,15 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
+
+
+
+# create all the models
+# migrate the db 
+# make assosiations
+# make controllers and routes 
+# make a show/index action for carted products
+# make sure only the user can see their cart
+# meaning they need to be logged in
+# make a create action for carted oroducts so users can add to cart 
+#  and a destroy to take things out
